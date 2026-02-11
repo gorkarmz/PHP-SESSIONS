7ae10932a53409d5eb859c0fdd10f3637091f58e
Gorka <gorkaramirez936@gmail.com>
2026-02-11 10:08:33 +0100

    Total price displayed on shop page is incorrect (ARREGLADO)

--- MP0487_RA4_Session_Shop/shop.php
+++ MP0487_RA4_Session_Shop/shop.php
@@ -48,8 +48,10 @@ if (isset($_POST['product'])) {
 
 // Calculate total price
 $totalPrice = 0;
-foreach ($_SESSION['cart'] as $item => $qty) {
-    $totalPrice = $products[$item]['price'] * $qty; 
+if (!empty($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $item => $qty) {
+        $totalPrice += $products[$item]['price'] * $qty;
+    }
 }
 ?>
 
